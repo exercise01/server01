d8f5f7b7d2d63a6612bfbd24d90369eb3eac7bf4
k.osawa <saataysu4408999@gmail.com>
2024-12-08 17:41:56 +0900

    initialize

---
+++ db-data.ts
@@ -0,0 +1,17 @@
+export const COURSES:any ={
+1:{
+    id:1,
+    description:"angular-core",
+    iconUrl: '',
+    longDescription:"The most important part for angular-core", 
+    category: 'BEGINER'
+},
+2:{
+    id:2,
+    description:"angular-form",
+    iconUrl: '',
+    longDescription:"The most important part for angular-form", 
+    category: 'ADVANCE'
+}
+
+}
